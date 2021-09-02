ae3685b68fe1f7bc27eb9425e38a4cb213a7f35e
Ivan Lesko <65163471+Ivanlsko@users.noreply.github.com>
2021-09-02 21:46:13 +0200

    basic version fully working

--- script.js
+++ script.js
@@ -1,55 +1,37 @@
-//const valuesArr = [3, 1, 19, 6, 16, 18, 7, 30, 21, 6, 2, 6, 7, 19, 20, 8, 21, 2, 20, 11, 30, 16, 2, 18, 4, 17, 22, 12, 19, 4, 2, 22, 2, 16, 28, 23, 25, 10, 11, 19];
+"use strict";
+
 const valuesArr = [];
-console.log(valuesArr);
 let newDiv;
-let theNumber;
+let theNumber = 1;
 let randomNumber;
 let arrLength = 0;
-
-function generateRanNumber() {
-  return Math.floor(Math.random() * 32);
-}
 
 loop();
 
 function loop() {
-  displayBars();
+  randomNumber = Math.floor(Math.random() * 32);
   arrLength++;
   if (arrLength < 40) {
-    randomNumber = generateRanNumber();
     valuesArr.push(randomNumber);
     console.log(valuesArr);
-    setTimeout(loop, 200);
+    setTimeout(loop, 100);
+    displayBars();
   } else {
-    valuesArr.shift();
-    randomNumber = generateRanNumber();
     valuesArr.push(randomNumber);
+    valuesArr.shift();
     console.log(valuesArr);
-    setTimeout(loop, 200);
+    setTimeout(loop, 100);
+    displayBars();
   }
 }
 
-//function addValueToArr() {}
-
-/*function displayBars() {
-  console.log("i am connected");
-  valuesArr.forEach((e) => {
-    newDiv = document.createElement("div");
-    theNumber = e;
-    document.querySelector("#chart").appendChild(newDiv);
-    addId();
-  });
-}*/
-
 function displayBars() {
   if (arrLength < 40) {
-    valuesArr[arrLength] = randomNumber;
     newDiv = document.createElement("div");
     document.querySelector("#chart").appendChild(newDiv);
     addId();
   } else {
     document.querySelector("#chart").firstChild.remove();
-    valuesArr[arrLength] = randomNumber;
     newDiv = document.createElement("div");
     document.querySelector("#chart").appendChild(newDiv);
     addId();
